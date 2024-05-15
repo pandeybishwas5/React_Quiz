6b704e42131762bae937b9f63069b9af3c39ca03
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-05-15 23:23:15 +1000

    Add timer to questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react"
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+
+
+
+    const quizCompleted = activeQuestionIndex === QUESTIONS.length
+    if (quizCompleted) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
@@ -11,10 +28,11 @@ export default function Quiz() {
 
     return (
         <div id="quiz">
-            <div id="questions">
+            <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
